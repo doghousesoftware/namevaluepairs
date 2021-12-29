8bfa9739295b3fea90351bf995e5656090fe9e06
bryan weinstein <bryanweinstein@outlook.com>
2021-12-29 13:56:53 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,42 @@
+enum RadioMessage {
+    message1 = 49434,
+    bogus1 = 6130
+}
+input.onButtonPressed(Button.A, function () {
+    if (input.lightLevel() > 150) {
+        radio.sendValue("light", input.lightLevel())
+    } else {
+        radio.sendValue("dark", input.lightLevel())
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendValue("oscar", 25)
+})
+radio.onReceivedValue(function (name, value) {
+    if (name == "light") {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    } else if (name == "dark") {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            `)
+    }
+})
+basic.showNumber(input.lightLevel())
